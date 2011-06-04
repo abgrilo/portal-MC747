d742cc4546de8ce4d47e7fffbe78023598d45af2
Douglas Alves <dnalves3@gmail.com>
2011-06-03 21:09:15 -0300

    Metodo pegando o soap do edilson

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  require 'savon'
+  
+  def edilson_method (cpf, token)
+    client = Savon::Client.new do
+      wsdl.document = "http://www.supercontrole.com/747/wsConsultaCliente.asmx?WSDL"
+    end
+    resp = client.request :wsdl, :consulta_cliente, {:token => token, :cpf => cpf}
+    resp.to_hash[:consulta_cliente_response][:consulta_cliente_result]
+  end
+
 end
